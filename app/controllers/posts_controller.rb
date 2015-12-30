85e0451fb6c1e9e2d9a61187c8334dd55e3310b9
Steve Smith <stevesmith2609@gmail.com>
2015-12-30 20:01:00 +0800

    Add random post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
 
   def show
     @comments = @post.comments.all
+    @random_post = Post.where.not(id: @post).order("RANDOM()").first
   end
 
   def new
